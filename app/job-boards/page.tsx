6d626f31a20bd007537406bd35827935ccf3dab7
Joshua Lehman <joshlehman.dev@gmail.com>
2024-02-19 11:08:58 -0500

    updates

--- app/job-boards/page.tsx
+++ app/job-boards/page.tsx
@@ -9,6 +9,94 @@ import { useState } from "react";
 
 import { Metadata } from "next";
 
+type Search = {
+  id: number;
+  name: string;
+  url: string | "";
+  searchUrl: string | "";
+  logoImage: string | "";
+};
+
+const searchProviders = [
+  {
+    id: 1,
+    name: "indeed",
+    url: "https://ca.indeed.com/",
+    searchUrl: "https://ca.indeed.com/jobs?q=",
+    logoImage: "/logo-indeed.svg",
+  },
+  {
+    id: 2,
+    name: "MONSTER",
+    url: "https://www.monster.ca/",
+    searchUrl: "https://www.monster.ca/jobs/search?q=",
+    logoImage: "/logo-monster.png",
+  },
+  {
+    id: 3,
+    name: "glassdoor",
+    url: "https://www.glassdoor.ca/",
+    searchUrl: "https://www.glassdoor.ca/Job/jobs.htm?sc.keyword=",
+    logoImage: "/logo-glassdoor.png",
+  },
+  {
+    id: 4,
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/",
+    searchUrl: "https://www.linkedin.com/jobs/search?keywords=",
+    logoImage: "/logo-linkedin.svg",
+  },
+  {
+    id: 5,
+    name: "Google",
+    url: "https://www.google.com/",
+    searchUrl: "https://www.google.com/search?q=&oq=&ibp=htl;jobs",
+    logoImage: "/logo-google.svg",
+  },
+  {
+    id: 6,
+    name: "jooble",
+    url: "https://jooble.org/",
+    searchUrl: "https://jooble.org/SearchResult?ukw=",
+    logoImage: "/logo-jooble.svg",
+  },
+  {
+    id: 7,
+    name: "SimplyHired",
+    url: "https://www.simplyhired.com/",
+    searchUrl: "https://www.simplyhired.com/search?q=",
+    logoImage: "logo-simply-hired.png",
+  },
+  {
+    id: 8,
+    name: "ZipRecruiter",
+    url: "https://www.ziprecruiter.com/",
+    searchUrl: "https://www.ziprecruiter.com/jobs-search?search=",
+    logoImage: "logo-zip-recruiter.svg",
+  },
+  {
+    id: 9,
+    name: "snagajob",
+    url: "https://www.snagajob.com/",
+    searchUrl: "https://www.snagajob.com/search?q=",
+    logoImage: "logo-snagajob.jpg",
+  },
+  {
+    id: 10,
+    name: "CAREERBUILDER",
+    url: "https://www.careerbuilder.com/",
+    searchUrl: "https://www.careerbuilder.com/jobs?emp=&keywords=",
+    logoImage: "logo-career-builder.png",
+  },
+  {
+    id: 11,
+    name: "idealist",
+    url: "https://www.idealist.org/",
+    searchUrl: "https://www.idealist.org/en/jobs?q=",
+    logoImage: "logo-idealist.png",
+  },
+];
+
 // export const metadata: Metadata = {
 //   title: "Resume Wrangler",
 //   description: "The official website of Resume Wrangler.",
@@ -63,47 +151,28 @@ export default function Page() {
               placeholder="e.g. Software Engineer"
             ></input>
             <div className="border-b-2 border-black h-4 w-full"></div>
-            <div className="border-b-2 border-black w-full">
-              {jobTitle ? (
-                <a href={`https://ca.indeed.com/jobs?q=${jobTitle}`}>
-                  <h2 className="py-2 text-3xl">indeed</h2>
-                </a>
-              ) : (
-                <a href="https://ca.indeed.com/jobs?q=software+engineer">
-                  <h2 className="py-2 text-3xl">indeed</h2>
-                </a>
-              )}
-            </div>
 
-            <div className="border-b-2 border-black w-full">
-              {jobTitle ? (
-                <a href={`https://www.monster.ca/jobs/search?q=${jobTitle}`}>
-                  <h2 className="py-2 text-3xl">MONSTER</h2>
-                </a>
+            {searchProviders.map((provider) =>
+              jobTitle ? (
+                <div
+                  key={provider.id}
+                  className="border-b-2 border-black w-full"
+                >
+                  <a href={`${provider.searchUrl}${jobTitle}`}>
+                    <h2 className="py-2 text-3xl">{provider.name}</h2>
+                  </a>
+                </div>
               ) : (
-                <a href="https://ca.indeed.com/jobs?q=software+engineer">
-                  <h2 className="py-2 text-3xl">MONSTER</h2>
-                </a>
-              )}
-            </div>
-            <div className="border-b-2 border-black w-full">
-              <h2 className="py-2 text-3xl">glassdoor</h2>
-            </div>
-            <div className="border-b-2 border-black w-full">
-              <h2 className="py-2 text-3xl">LinkedIn</h2>
-            </div>
-            <div className="border-b-2 border-black w-full">
-              <h2 className="py-2 text-3xl">Google</h2>
-            </div>
-            <div className="border-b-2 border-black w-full">
-              <h2 className="py-2 text-3xl">Jooble</h2>
-            </div>
-            <div className="border-b-2 border-black w-full">
-              <h2 className="py-2 text-3xl">Simply Hired</h2>
-            </div>
-            <div className="border-b-2 border-black w-full">
-              <h2 className="py-2 text-3xl">Zip Recruiter</h2>
-            </div>
+                <div
+                  key={provider.id}
+                  className="border-b-2 border-black w-full"
+                >
+                  <a key={provider.id} href={provider.url}>
+                    <h2 className="py-2 text-3xl">{provider.name}</h2>
+                  </a>
+                </div>
+              )
+            )}
           </div>
         </div>
       </main>
