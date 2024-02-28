91b62a4ab876b8d22f50bec0416831a93b5c59b5
Joshua Lehman <joshlehman.dev@gmail.com>
2024-02-27 23:27:42 -0500

    updates

--- app/dashboard/resume-templates/page.tsx
+++ app/dashboard/resume-templates/page.tsx
@@ -439,7 +439,6 @@ export default function ClassicResume() {
                 </label>
               </div>
             </div>
-            <div style={{ height: "0.5rem" }}></div>
           </div>
         </div>
       </div>
@@ -517,17 +516,6 @@ export default function ClassicResume() {
               </div>
             </div>
             <div className="flex flex-col">
-              <div className="flex flex-row">
-                <div className="px-1 flex align-middle">
-                  <input
-                    className="m-auto bg-slate-200 rounded"
-                    type="checkbox"
-                  ></input>
-                </div>
-                <div className="flex flex-col">
-                  <p>Show skills section?</p>
-                </div>
-              </div>
               <div className="flex flex-row py-2">
                 <div className="px-1 flex align-middle">
                   <input
@@ -537,6 +525,17 @@ export default function ClassicResume() {
                 </div>
                 <div className="flex flex-col">
                   <p>Show skills progress bars?</p>
+                </div>
+              </div>
+              <div className="flex flex-row">
+                <div className="px-1 flex align-middle">
+                  <input
+                    className="m-auto bg-slate-200 rounded"
+                    type="checkbox"
+                  ></input>
+                </div>
+                <div className="flex flex-col">
+                  <p>Show skills section?</p>
                 </div>
               </div>
             </div>
@@ -682,7 +681,6 @@ export default function ClassicResume() {
                 </div>
               </div>
             </div>
-            <div style={{ height: "0.5rem" }}></div>
           </div>
         </div>
         <div className="py-2"></div>
@@ -690,10 +688,10 @@ export default function ClassicResume() {
     );
   };
 
-  const YourWorkExperience = () => {
+  const YourWorkExperiences = () => {
     return (
       <div>
-        <div className="your-education">
+        <div className="your-work-experiences">
           <div className="flex flex-row justify-between">
             <div className="flex flex-col">
               <div className="py-2 font-bold text-xl">
@@ -784,15 +782,244 @@ export default function ClassicResume() {
                   </div>
                   <div className="flex flex-row w-auto">
                     <div className="flex flex-col w-full py-1 px-1">
-                      <label className="py-1" htmlFor="degree">
-                        Degree
+                      <label className="py-1" htmlFor="description">
+                        Description
+                      </label>
+                      <textarea
+                        id="description"
+                        className="rounded bg-slate-200"
+                        value={resumeStyling.resumeTitle}
+                        placeholder="Description"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="flex flex-row justify-end py-2 px-2 font-bold">
+                    <p>Delete</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col w-full py-2 px-1">
+              <button
+                className="rounded bg-amber-300 h-10 border border-black"
+                value={resumeStyling.resumeTemplate}
+                // onChange={(e) => resumeTemplateAction(e)}
+                name="add-experience"
+                id="add-experience"
+              >
+                Add Work Experience
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="py-2"></div>
+      </div>
+    );
+  };
+
+  const YourOrganizations = () => {
+    return (
+      <div>
+        <div className="your-organizations">
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col">
+              <div className="py-2 font-bold text-xl">
+                <h2>Organization</h2>
+              </div>
+            </div>
+            <div className="flex flex-col ">
+              <div className="flex flex-row m-auto">
+                <div className="flex flex-col px-4">Move Up</div>
+                <div className="flex flex-col">Move Down</div>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-md border-[1px] border-slate-300 rounded px-5 py-2 ">
+            <div className="flex flex-row w-auto">
+              <div className="flex flex-col w-full py-1 px-1">
+                <label className="py-1" htmlFor="section-title">
+                  Section Title
+                </label>
+                <input
+                  id="section-title"
+                  className="rounded bg-slate-200"
+                  value={resumeStyling.resumeTitle}
+                  onChange={(e) => resumeTitleAction(e)}
+                  placeholder="Section Title"
+                ></input>
+              </div>
+            </div>
+            <div className="flex flex-row w-auto">
+              <div className="flex flex-col w-full py-1 px-1">
+                <label className="py-1" htmlFor="organization">
+                  Organizations
+                </label>
+                <div className="rounded border border-black w-full px-2">
+                  <div className="flex flex-row w-auto">
+                    <div className="flex flex-col w-full py-1 px-1">
+                      <label className="py-1" htmlFor="organization-name">
+                        Name
                       </label>
                       <input
-                        id="degree"
+                        id="organization-name"
                         className="rounded bg-slate-200"
                         value={resumeStyling.resumeTitle}
                         onChange={(e) => resumeTitleAction(e)}
-                        placeholder="Degree"
+                        placeholder="Title, Activity, name, etc.."
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="flex flex-row w-auto">
+                    <div className="flex flex-col w-full py-1 px-1">
+                      <label className="py-1" htmlFor="organization-location">
+                        Location
+                      </label>
+                      <input
+                        id="organization-location"
+                        className="rounded bg-slate-200"
+                        placeholder="Location"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="flex flex-row w-auto">
+                    <div className="flex flex-col w-full py-1 px-1">
+                      <label className="py-1" htmlFor="organization-start-date">
+                        Start Date
+                      </label>
+                      <input
+                        id="organization-start-date"
+                        className="rounded bg-slate-200"
+                        placeholder="Start Date"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="flex flex-row w-auto">
+                    <div className="flex flex-col w-full py-1 px-1">
+                      <label className="py-1" htmlFor="organization-end-date">
+                        End Date
+                      </label>
+                      <input
+                        id="organization-end-date"
+                        className="rounded bg-slate-200"
+                        placeholder="End Date"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="flex flex-row w-auto">
+                    <div className="flex flex-col w-full py-1 px-1">
+                      <label
+                        className="py-1"
+                        htmlFor="organization-description"
+                      >
+                        Description
+                      </label>
+                      <textarea
+                        id="organization-description"
+                        className="rounded bg-slate-200"
+                        placeholder="Description"
+                      ></textarea>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-row justify-end py-2 px-2 font-bold">
+                    <p>Delete</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col w-full py-2 px-1">
+              <button
+                className="rounded bg-amber-300 h-10 border border-black"
+                value={resumeStyling.resumeTemplate}
+                // onChange={(e) => resumeTemplateAction(e)}
+                name="add-organization"
+                id="add-organization"
+              >
+                Add Organization
+              </button>
+            </div>
+            <div className="flex flex-row py-2">
+              <div className="px-1 flex align-middle">
+                <input
+                  className="m-auto bg-slate-200 rounded"
+                  type="checkbox"
+                ></input>
+              </div>
+              <div className="flex flex-col">
+                <p>Show organization section?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="py-2"></div>
+      </div>
+    );
+  };
+
+  const YourCertifications = () => {
+    return (
+      <div>
+        <div className="your-certifications">
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col">
+              <div className="py-2 font-bold text-xl">
+                <h2>Certification</h2>
+              </div>
+            </div>
+            <div className="flex flex-col ">
+              <div className="flex flex-row m-auto">
+                <div className="flex flex-col px-4">Move Up</div>
+                <div className="flex flex-col">Move Down</div>
+              </div>
+            </div>
+          </div>
+          <div className="drop-shadow-md border-[1px] border-slate-300 rounded px-5 py-2 ">
+            <div className="flex flex-row w-auto">
+              <div className="flex flex-col w-full py-1 px-1">
+                <label className="py-1" htmlFor="company-name">
+                  Section Title
+                </label>
+                <input
+                  id="section-title"
+                  className="rounded bg-slate-200"
+                  value={resumeStyling.resumeTitle}
+                  onChange={(e) => resumeTitleAction(e)}
+                  placeholder="Section Title"
+                ></input>
+              </div>
+            </div>
+            <div className="flex flex-row w-auto">
+              <div className="flex flex-col w-full py-1 px-1">
+                <label className="py-1" htmlFor="work-certification">
+                  Work Certification
+                </label>
+                <div className="rounded border border-black w-full px-2">
+                  <div className="flex flex-row w-auto">
+                    <div className="flex flex-col w-full py-1 px-1">
+                      <label className="py-1" htmlFor="certification-name">
+                        Name
+                      </label>
+                      <input
+                        id="certification-name"
+                        className="rounded bg-slate-200"
+                        value={resumeStyling.resumeTitle}
+                        onChange={(e) => resumeTitleAction(e)}
+                        placeholder="Certification Name"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="flex flex-row w-auto">
+                    <div className="flex flex-col w-full py-1 px-1">
+                      <label className="py-1" htmlFor="certification-date">
+                        Date
+                      </label>
+                      <input
+                        id="certification-date"
+                        className="rounded bg-slate-200"
+                        value={resumeStyling.resumeTitle}
+                        onChange={(e) => resumeTitleAction(e)}
+                        placeholder="Certification Date"
                       ></input>
                     </div>
                   </div>
@@ -807,28 +1034,26 @@ export default function ClassicResume() {
                 className="rounded bg-amber-300 h-10 border border-black"
                 value={resumeStyling.resumeTemplate}
                 // onChange={(e) => resumeTemplateAction(e)}
-                name="add-school"
-                id="add-school"
+                name="add-certification"
+                id="add-certification"
               >
-                Add School
+                Add Certification
               </button>
             </div>
-            <div className="flex flex-col">
-              <div className="flex flex-row py-2">
-                <div className="px-1 flex align-middle">
-                  <input
-                    className="m-auto bg-slate-200 rounded"
-                    type="checkbox"
-                  ></input>
-                </div>
-                <div className="flex flex-col">
-                  <p>Show education section?</p>
-                </div>
+            <div className="flex flex-row py-2">
+              <div className="px-1 flex align-middle">
+                <input
+                  className="m-auto bg-slate-200 rounded"
+                  type="checkbox"
+                ></input>
+              </div>
+              <div className="flex flex-col">
+                <p>Show certification section?</p>
               </div>
             </div>
-            <div style={{ height: "0.5rem" }}></div>
           </div>
         </div>
+
         <div className="py-2"></div>
       </div>
     );
@@ -844,7 +1069,9 @@ export default function ClassicResume() {
           <div className="py-2"></div>
           <YourSkills />
           <YourEducation />
-          <YourWorkExperience />
+          <YourWorkExperiences />
+          <YourOrganizations />
+          <YourCertifications />
           <div className="p-2 text-center">
             <a
               href={`/api/pdf?bodyFont=${resumeStyling.bodyFont}&headerFont=${resumeStyling.headingFont}`}
