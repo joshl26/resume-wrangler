bc68457a0c1aaf75b1ca5597519ac92f8c733277
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-04 12:06:20 -0500

    updates

--- app/dashboard/resume-templates/page.tsx
+++ app/dashboard/resume-templates/page.tsx
@@ -2,9 +2,24 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="w-full h-full pl-3 pr-4 ">
-      <div className="border border-red w-full h-full">
-        <h1>Résumé Templates Page</h1>
+    <div className="w-full h-full pl-3 pr-4">
+      <h1 className="">Start with a Résumé Template</h1>
+      <p>
+        Get a quick start by customizing an existing resume example template.
+      </p>
+      <div className="border border-black w-full h-full rounded">
+        <div className="flex">
+          <div className="flex flex-row">
+            <div className="flex flex-column">
+              <div className="flex flex-row"></div>
+              <div className="flex flex-row"></div>
+              <div className="flex flex-row"></div>
+            </div>
+            <div className="flex flex-column"></div>
+            <div className="flex flex-column"></div>
+            <div className="flex flex-column"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
