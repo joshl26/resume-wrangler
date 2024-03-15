2b3644bba9e4611201036912276c11138fe2e8f2
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-15 10:51:34 -0400

    udpates

--- app/api/pdf/route.ts
+++ app/api/pdf/route.ts
@@ -2,7 +2,7 @@
 import { NextRequest } from "next/server";
 // import { PuppeteerNode } from "puppeteer";
 
-const chrome = require("chrome-aws-lambda");
+const chrome = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
 //   puppeteer = require("puppeteer-core");
 // import chrome from "chrome-aws-lambda";
@@ -40,23 +40,6 @@ export async function GET(req: NextRequest) {
     const resumeId = params.resumeId as string;
     const userEmail = params.userEmail as string;
 
-    // const options = process.env.AWS_REGION
-    //   ? {
-    //       args: chrome.args,
-    //       executablePath: await chrome.executablePath,
-    //       headless: chrome.headless,
-    //     }
-    //   : {
-    //       args: [
-    //         "--no-sandbox",
-    //         "--disable-web-security",
-    //         "--font-render-hinting=none",
-    //         "--force-color-profile=srgb",
-    //       ],
-    //       executablePath:
-    //         "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-    //     };
-
     const options = process.env.AWS_REGION
       ? {
           args: chrome.args,
@@ -67,10 +50,41 @@ export async function GET(req: NextRequest) {
           headless: true,
           ignoreHTTPSErrors: true,
           args: [
+            "--autoplay-policy=user-gesture-required",
+            "--disable-background-networking",
+            "--disable-background-timer-throttling",
+            "--disable-backgrounding-occluded-windows",
+            "--disable-breakpad",
+            "--disable-client-side-phishing-detection",
+            "--disable-component-update",
+            "--disable-default-apps",
+            "--disable-dev-shm-usage",
+            "--disable-domain-reliability",
+            "--disable-extensions",
+            "--disable-features=AudioServiceOutOfProcess",
+            "--disable-hang-monitor",
+            "--disable-ipc-flooding-protection",
+            "--disable-notifications",
+            "--disable-offer-store-unmasked-wallet-cards",
+            "--disable-popup-blocking",
+            "--disable-print-preview",
+            "--disable-prompt-on-repost",
+            "--disable-renderer-backgrounding",
+            "--disable-setuid-sandbox",
+            "--disable-speech-api",
+            "--disable-sync",
+            "--hide-scrollbars",
+            "--ignore-gpu-blacklist",
+            "--metrics-recording-only",
+            "--mute-audio",
+            "--no-default-browser-check",
+            "--no-first-run",
+            "--no-pings",
             "--no-sandbox",
-            "--disable-web-security",
-            "--font-render-hinting=none",
-            "--force-color-profile=srgb",
+            "--no-zygote",
+            "--password-store=basic",
+            "--use-gl=swiftshader",
+            "--use-mock-keychain",
           ],
           executablePath:
             "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
@@ -78,40 +92,10 @@ export async function GET(req: NextRequest) {
 
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
-    await page.setViewport({ width: 2000, height: 1000 });
-    await page.goto(
-      `http://${process.env.DEPLOYMENT_URL}/resume/${resumeId}/${userEmail}`,
-      { waitUntil: "networkidle0" }
+
+    await page.setUserAgent(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
     );
-    return await page.screenshot({ type: "png" });
-
-    // const browser = await puppeteer.launch(options);
-
-    // const page = await browser.newPage();
-
-    // await page.setViewport({ width: 2000, height: 1000 });
-    // await page.setUserAgent(
-    //   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
-    // );
-
-    //   const browser = await puppeteer.launch({
-    //     // headless: true,
-    //     // args: [
-    //     //   "--no-sandbox",
-    //     //   "--disable-web-security",
-    //     //   "--font-render-hinting=none",
-    //     //   "--force-color-profile=srgb",
-    //     // ],
-    //     args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-    //     defaultViewport: chrome.defaultViewport,
-    //     executablePath: chrome.executablePath,
-    //     headless: true,
-    //     ignoreHTTPSErrors: true,
-    //   });
-    //   const page = await browser.newPage();
-    //   await page.setUserAgent(
-    //     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
-    //   );
 
     //   // await page.goto("http://localhost:3000/login");
 
@@ -122,27 +106,27 @@ export async function GET(req: NextRequest) {
 
     //   // await page.waitForNavigation(); // <------------------------- Wait for Navigation
 
-    // await page.goto(
-    //   `http://${process.env.DEPLOYMENT_URL}/resume/${resumeId}/${userEmail}`,
-    //   {
-    //     waitUntil: "networkidle0",
-    //   }
-    // );
+    await page.goto(
+      `http://${process.env.DEPLOYMENT_URL}/resume/${resumeId}/${userEmail}`,
+      {
+        waitUntil: "networkidle0",
+      }
+    );
 
-    // await page.emulateMediaType("print");
+    await page.emulateMediaType("print");
 
-    // await page.evaluateHandle("document.fonts.ready");
+    await page.evaluateHandle("document.fonts.ready");
 
-    // const buffer = await page.pdf({
-    //   displayHeaderFooter: false,
-    //   // format: "a4",
-    //   format: "letter",
-    //   printBackground: true,
-    // });
+    const buffer = await page.pdf({
+      displayHeaderFooter: false,
+      // format: "a4",
+      format: "letter",
+      printBackground: true,
+    });
 
-    // browser.close();
+    browser.close();
 
-    // return new Response(buffer, { headers: { "content-type": "image/png" } });
+    return new Response(buffer, { headers: { "content-type": "image/png" } });
 
     // Close browser **after** we returned the PDF to the caller.
   } catch (error) {
--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "puppeteer-core",
+      "@sparticuz/chromium-min",
+    ],
+  },
   images: {
     remotePatterns: [
       {
