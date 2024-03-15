239831378437c09c42eecd2ad3f81dafa6d2aa12
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-14 22:25:48 -0400

    updates

--- app/ui/edit-button.tsx
+++ app/ui/edit-button.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const EditButton = ({ resumeId }: { resumeId: any }) => {
+const EditButton = () => {
   return (
     <a
-      href={`/dashboard/resume/edit/${resumeId}`}
+      href={`/dashboard/resume/edit/`}
       className="hover:bg-slate-600 text-white flex absolute bottom-6 right-6 w-[100px] h-[100px] bg-blue-500 border-2 border-black rounded-full"
     >
       <p className="font-bold text-lg w-full flex justify-center m-auto">
