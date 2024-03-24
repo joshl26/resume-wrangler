043a08fca8d10d82d1273daf350fbc59f770c4cc
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-24 16:03:37 -0400

    updates

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,47 +1,50 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { ReactNode } from "react";
-import ResumeWranglerIcon from "/public/ResumeWranglerLogo.png";
+import ResumeWranglerIcon from "/public/ResumeWranglerLogo-white.png";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 const Landing = ({ children }: { children: ReactNode }) => {
   return (
-    <>
-      <div className="min-w-full bg-gray-50">
-        <header className="flex flex-row h-20">
-          <nav className="max-w-screen-xl w-full m-auto flex justify-between">
-            <div className="h-[50px] w-[50px]">
-              <Image
-                className="rounded"
-                width={0}
-                height={0}
-                alt=""
-                src={ResumeWranglerIcon}
-              />
+    <div className="bg-gray-50 h-[3000px]">
+      <header className="bg-gray-50 w-screen h-20 justify-between fixed t-0 pt-4">
+        <nav className=" max-w-screen-xl w-auto  m-auto ">
+          <div className="flex flex-row justify-between w-full">
+            <div className="flex flex-col w-1/2">
+              <div className="h-[50px] w-[50px]">
+                <Image
+                  className="rounded opacity-70"
+                  width={0}
+                  height={0}
+                  alt=""
+                  src={ResumeWranglerIcon}
+                />
+              </div>
             </div>
-            {/* <AcmeLogo /> */}
-            <div className="text-center flex gap-4">
-              <Link
-                href="/register"
-                className="flex items-center gap-5 self-start rounded-lg bg-[#FC440F] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-              >
-                <span>Create Account</span>
-                <ArrowRightIcon className="w-5 md:w-6" />
-              </Link>
-              <Link
-                href="/login"
-                className="flex items-center gap-5 self-start rounded-lg bg-[#FC440F] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-              >
-                <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-              </Link>
+            <div className="flex flex-col w-1/2">
+              <div className="flex flex-row justify-end gap-2">
+                <Link
+                  href="/register"
+                  className="w-auto rounded-full bg-gray-50 px-6 py-3  "
+                >
+                  Create Account
+                </Link>
+                <Link
+                  href="/login"
+                  className="rounded-full bg-rose-600 px-6 py-3 font-medium text-white"
+                >
+                  Log in
+                </Link>
+              </div>
             </div>
-          </nav>
-        </header>
-        <main className="min-h-screen max-w-screen-xl w-full m-auto flex ">
-          {children}
-        </main>
-      </div>
-    </>
+          </div>
+          {/* <AcmeLogo /> */}
+        </nav>
+      </header>
+      <main className="min-h-screen max-w-screen-xl w-full m-auto flex ">
+        {children}
+      </main>
+    </div>
   );
 };
 
