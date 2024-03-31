3b01f3d0def277ca43696bf75bde28418f331131
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-31 15:10:15 -0400

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,10 +6,20 @@ import { defineConfig, devices } from "@playwright/test";
  */
 require("dotenv").config();
 
+// Use process.env.PORT by default and fallback to port 3000
+const PORT = process.env.PORT || 3000;
+
+// Set webServer.url and use.baseURL with the location of the WebServer respecting the correct set port
+const baseURL = `http://localhost:${PORT}`;
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  // Timeout per test
+  timeout: 30 * 1000,
+  // Artifacts folder where screenshots, videos, and traces are stored.
+  outputDir: "test-results/",
   testDir: "./playwright-tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -24,7 +34,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `${process.env.DEPLOYMENT_URL}`,
+    baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -48,13 +58,13 @@ export default defineConfig({
     // },
 
     /* Test against mobile viewports. */
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
     // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
+    //   name: "Mobile Safari",
+    //   use: { ...devices["iPhone 12"] },
     // },
 
     /* Test against branded browsers. */
@@ -69,9 +79,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: baseURL,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
