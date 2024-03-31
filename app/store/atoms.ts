7aaf5488516fbdeda05fe6d937e4732905d12ee6
Joshua Lehman <joshlehman.dev@gmail.com>
2024-03-30 22:56:35 -0400

    ts-updates

--- app/store/atoms.ts
+++ app/store/atoms.ts
@@ -1,9 +1,4 @@
 import { atom } from "jotai";
-import { Cart, Review, ResumeTemplate } from "@/app/api/types";
-
-export const cartAtom = atom<Cart>({
-  products: [],
-});
-export const reviewsAtom = atom<Review[] | null>(null);
+import { ResumeTemplate } from "@/app/api/types";
 
 export const resumeTemplateAtom = atom<ResumeTemplate>({ styles: [] });
