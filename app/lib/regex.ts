2a10e503de50b1e3f0c5a6083e738ef5f8c47961
Joshua Lehman <joshlehman.dev@gmail.com>
2024-04-21 00:31:40 -0400

    signup-form-updates

--- app/lib/regex.ts
+++ app/lib/regex.ts
@@ -2,15 +2,17 @@
 // - At least minimum six characters to a maximum of 20.
 // - Can be a combination of lower case, upper case and numbers ONLY no special characters
 // export const usernameRegex = new RegExp("^[A-Za-z][A-Za-z0-9_]{3,20}$");
-export const usernameRegex = "^[A-Za-z][A-Za-z0-9_]{3,20}$";
+export const usernameRegex = "^[A-Za-z][A-Za-z0-9_]{5,20}$";
 
 // Creates a Regex expression that validates password string on the following:
 // - Can be a combination of lower case, upper case and numbers ONLY no special characters
 // - name and domain seperated by @ symbol
+
 // export const emailRegex = new RegExp(
 //   "^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,})$"
 // );
-export const emailRegex = "^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,})$";
+export const emailRegex =
+  "[[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
 
 // Creates a Regex expression that validates password string on the following:
 // - At least one upper case A-Z
@@ -18,9 +20,20 @@ export const emailRegex = "^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,})$";
 // - At least one digit 0-9
 // - At least one special character
 // - Minimum six characters in length
-// export const passwordRegex = new RegExp(
-//   "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$"
-// );
 
 export const passwordRegex =
   "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$";
+
+export const atLeastOneLowerCaseEnglishRegex = "(?=.*?[a-z])";
+
+export const atLeastOneUpperCaseEnglishRegex = "(?=.*?[A-Z])";
+
+export const atLeastOneNumberZeroToNineRegex = "(?=.*?[0-9])";
+
+export const atLeastOneSpecialCharacterRegex = "(?=.*?[#?!@$%^&*-])";
+
+export const atLeastSixCharactersRegex = ".{6,}";
+
+export const lettersAndNumbersRegex = "^([a-zA-Z0-9_.-]*)$";
+
+export const noWhiteSpaceRegex = "^((?!s).)*$";
--- app/ui/forms/register-user.tsx
+++ app/ui/forms/register-user.tsx
@@ -6,10 +6,24 @@ import { redirect } from "next/navigation";
 import { SubmitButton } from "../submit-button";
 import BackButton from "../back-button";
 import { CreateNewUser } from "@/app/lib/actions";
-import { emailRegex, passwordRegex, usernameRegex } from "@/app/lib/regex";
+import {
+  atLeastOneLowerCaseEnglishRegex,
+  atLeastOneUpperCaseEnglishRegex,
+  atLeastSixCharactersRegex,
+  emailRegex,
+  atLeastOneNumberZeroToNineRegex,
+  passwordRegex,
+  usernameRegex,
+  lettersAndNumbersRegex,
+  atLeastOneSpecialCharacterRegex,
+  noWhiteSpaceRegex,
+} from "@/app/lib/regex";
 import Link from "next/link";
 import AzureBlob from "../landing/azure-blob";
 import OrangeBlob from "../landing/orange-blob";
+import clsx from "clsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 
 export default function RegisterUser() {
   const [error, setError] = useState("");
@@ -22,6 +36,17 @@ export default function RegisterUser() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [sixCharacters, setSixCharacters] = useState(false);
+  const [sixCharacters2, setSixCharacters2] = useState(false);
+
+  const [oneLowerCaseEnglish, setOneLowerCaseEnglish] = useState(false);
+  const [oneUpperCaseEnglish, setOneUpperCaseEnglish] = useState(false);
+  const [onlyLettersAndNumbers, setOnlyLettersAndNumbers] = useState(false);
+  const [oneNumber, setOneNumber] = useState(false);
+  const [oneSpecialChar, setOneSpecialChar] = useState(false);
+  const [noWhiteSpace, setNoWhiteSpace] = useState(false);
 
   useEffect(() => {
     if (usernameValidated && emailValidated && passwordValidated) {
@@ -34,14 +59,32 @@ export default function RegisterUser() {
   const usernameOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     var usernameInput = e.target.value;
 
-    var re = new RegExp(usernameRegex);
-
     setUsername(usernameInput);
 
-    if (re.test(usernameInput)) {
+    var UserNameRegex = new RegExp(usernameRegex);
+
+    if (UserNameRegex.test(usernameInput)) {
       setUsernameValidated(true);
     } else {
       setUsernameValidated(false);
+    }
+
+    var SixCharactersRegex = new RegExp(atLeastSixCharactersRegex);
+
+    if (SixCharactersRegex.test(usernameInput)) {
+      setSixCharacters(true);
+    } else {
+      setSixCharacters(false);
+    }
+
+    var OnlyLettersAndNumbersRegex = new RegExp(lettersAndNumbersRegex);
+
+    console.log(OnlyLettersAndNumbersRegex.test(usernameInput));
+
+    if (OnlyLettersAndNumbersRegex.test(usernameInput)) {
+      setOnlyLettersAndNumbers(true);
+    } else {
+      setOnlyLettersAndNumbers(false);
     }
   };
 
@@ -69,7 +112,63 @@ export default function RegisterUser() {
     } else {
       setPasswordValidated(false);
     }
+
+    var OneUpperCaseEnglishRegex = new RegExp(atLeastOneUpperCaseEnglishRegex);
+
+    if (OneUpperCaseEnglishRegex.test(passwordInput)) {
+      setOneUpperCaseEnglish(true);
+    } else {
+      setOneUpperCaseEnglish(false);
+    }
+
+    var OneLowerCaseEnglishRegex = new RegExp(atLeastOneLowerCaseEnglishRegex);
+
+    if (OneLowerCaseEnglishRegex.test(passwordInput)) {
+      setOneLowerCaseEnglish(true);
+    } else {
+      setOneLowerCaseEnglish(false);
+    }
+
+    var OneNumberRegex = new RegExp(atLeastOneNumberZeroToNineRegex);
+
+    if (OneNumberRegex.test(passwordInput)) {
+      setOneNumber(true);
+    } else {
+      setOneNumber(false);
+    }
+
+    var OneSpecialCharRegex = new RegExp(atLeastOneSpecialCharacterRegex);
+
+    if (OneSpecialCharRegex.test(passwordInput)) {
+      setOneSpecialChar(true);
+    } else {
+      setOneSpecialChar(false);
+    }
+
+    var SixCharactersRegex = new RegExp(atLeastSixCharactersRegex);
+
+    if (SixCharactersRegex.test(passwordInput)) {
+      setSixCharacters2(true);
+    } else {
+      setSixCharacters2(false);
+    }
+
+    var NoWhiteSpaceRegex = new RegExp(noWhiteSpaceRegex);
+
+    if (NoWhiteSpaceRegex.test(passwordInput)) {
+      setNoWhiteSpace(true);
+    } else {
+      setNoWhiteSpace(false);
+    }
   };
+
+  function onClickHandler() {
+    if (showPassword === false) {
+      setShowPassword(true);
+    } else {
+      setShowPassword(false);
+    }
+  }
 
   return (
     <div className="relative flex flex-col h-[92vh] justify-center overflow-x-hidden overflow-clip">
@@ -83,35 +182,72 @@ export default function RegisterUser() {
         <div className="flex flex-row justify-around z-10">
           <form
             action={CreateNewUser}
-            className="flex flex-col form-amber rounded-lg w-auto md:w-[450px] gap-2 tight-shadow p-10 mb-3"
+            className="flex flex-col relative form-amber rounded-lg w-auto md:w-[450px] gap-2 tight-shadow p-10 mb-3"
           >
+            {/* <h1>Six Chars: {sixCharacters === false ? "false" : "true"}</h1>
+            <h1>
+              One Lower Case: {oneLowerCaseEnglish === false ? "false" : "true"}
+            </h1>
+            <h1>
+              One Upper Case: {oneUpperCaseEnglish === false ? "false" : "true"}
+            </h1>
+            <h1>One Number: {oneNumber === false ? "false" : "true"}</h1>
+            <h1>No White Space: {noWhiteSpace === false ? "false" : "true"}</h1>
+
+            <h1>
+              Letters and numbers:{" "}
+              {onlyLettersAndNumbers === false ? "false" : "true"}
+            </h1>
+            <h1>Username Input: {username}</h1>
+ */}
             <label className="font-bold" htmlFor="username">
               Username
             </label>
-            <input
-              disabled={pending}
-              name="username"
-              className="border border-slate-300 rounded"
-              type="text"
-              required
-              placeholder="Type your Username"
-              autoComplete="username"
-              onChange={usernameOnChangeHandler}
-              minLength={3}
-              maxLength={20}
-              value={username}
-            />
+            <div className="flex flex-row items-center">
+              <input
+                disabled={pending}
+                name="username"
+                className="border border-slate-300 rounded w-full"
+                type="text"
+                required
+                placeholder="Type your Username"
+                onChange={usernameOnChangeHandler}
+                minLength={3}
+                maxLength={20}
+                value={username}
+              />
+            </div>
+
             {usernameValidated === false && (
-              <div className="py-1">
+              <div className="py-1 mt-2 bg-amber-50 tight-shadow p-2 rounded">
                 <p className="font-medium">A valid username will:</p>
                 <ul>
-                  <li className="font-lite italic">
+                  <li
+                    className={clsx(
+                      "italic font-lite",
+                      sixCharacters ? "text-emerald-400" : " text-rose-500"
+                    )}
+                  >
                     - Have six to twenty characters
                   </li>
-                  <li className="font-lite italic">
+                  <li
+                    className={clsx(
+                      "italic font-lite",
+                      onlyLettersAndNumbers
+                        ? "text-emerald-400"
+                        : " text-rose-500"
+                    )}
+                  >
                     - Contain only A-Z, a-z and 0-9 characters
                   </li>
-                  <li className="font-lite italic">
+                  <li
+                    className={clsx(
+                      "italic font-lite",
+                      onlyLettersAndNumbers
+                        ? "text-emerald-400"
+                        : " text-rose-500"
+                    )}
+                  >
                     - Not contain spaces or special characters
                   </li>
                 </ul>
@@ -125,9 +261,8 @@ export default function RegisterUser() {
                 <input
                   disabled={pending}
                   name="email"
-                  className="border border-slate-300 rounded"
+                  className="border border-slate-300 rounded w-full"
                   type="email"
-                  autoComplete="email"
                   required
                   placeholder="Type your email address"
                   onChange={emailOnChangeHandler}
@@ -135,7 +270,7 @@ export default function RegisterUser() {
                 />
               </>
             )}
-            {usernameValidated && !emailValidated ? (
+            {/* {usernameValidated && !emailValidated ? (
               <div className="py-1">
                 <p className="font-medium">A valid email will:</p>
                 <ul className="font-lite italic">
@@ -146,37 +281,92 @@ export default function RegisterUser() {
               </div>
             ) : (
               ""
-            )}
+            )} */}
 
             {emailValidated && (
               <>
                 <label className="font-bold" htmlFor="email">
                   Password
                 </label>
-                <input
-                  disabled={pending}
-                  name="password"
-                  className="border border-slate-300 rounded"
-                  type="password"
-                  required
-                  autoComplete="new-password"
-                  placeholder="Type your password"
-                  onChange={passwordOnChangeHandler}
-                  minLength={6}
-                  maxLength={20}
-                  value={password}
-                />
+                <div className="">
+                  <input
+                    disabled={pending}
+                    name="password"
+                    className="border border-slate-300 rounded w-full"
+                    type={clsx(showPassword === true ? "text" : "password")}
+                    required
+                    placeholder="Type your password"
+                    onChange={passwordOnChangeHandler}
+                    minLength={6}
+                    maxLength={20}
+                    value={password}
+                  />
+                  <span className="absolute right-[52px] ">
+                    {showPassword === false ? (
+                      <FontAwesomeIcon
+                        onClick={onClickHandler}
+                        className="h-[20px] pt-3"
+                        icon={faEye}
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        onClick={onClickHandler}
+                        className="h-[20px] pt-3"
+                        icon={faEyeSlash}
+                      />
+                    )}
+                  </span>
+                </div>
               </>
             )}
             {usernameValidated && emailValidated && !passwordValidated ? (
-              <div className="py-1">
+              <div className="py-1 mt-2 bg-amber-50 p-2 rounded tight-shadow">
                 <p className="font-medium">A valid password will:</p>
                 <ul className="font-lite italic">
-                  <li>- Contain at least one upper case A-Z</li>
-                  <li>- Contain at least one lower case a-z</li>
-                  <li>- At least one digit from 0-9</li>
-                  <li>- At least one special character</li>
-                  <li>- Minimum six, to max twenty characters</li>
+                  <li
+                    className={clsx(
+                      "italic font-lite",
+                      oneUpperCaseEnglish
+                        ? "text-emerald-400"
+                        : " text-rose-500"
+                    )}
+                  >
+                    - Contain at least one upper case A-Z
+                  </li>
+                  <li
+                    className={clsx(
+                      "italic font-lite",
+                      oneLowerCaseEnglish
+                        ? "text-emerald-400"
+                        : " text-rose-500"
+                    )}
+                  >
+                    - Contain at least one lower case a-z
+                  </li>
+                  <li
+                    className={clsx(
+                      "italic font-lite",
+                      oneNumber ? "text-emerald-400" : " text-rose-500"
+                    )}
+                  >
+                    - At least one digit from 0-9
+                  </li>
+                  <li
+                    className={clsx(
+                      "italic font-lite",
+                      oneSpecialChar ? "text-emerald-400" : " text-rose-500"
+                    )}
+                  >
+                    - At least one special character
+                  </li>
+                  <li
+                    className={clsx(
+                      "italic font-lite",
+                      sixCharacters2 ? "text-emerald-400" : " text-rose-500"
+                    )}
+                  >
+                    - Minimum six, to max twenty characters
+                  </li>
                 </ul>
               </div>
             ) : (
